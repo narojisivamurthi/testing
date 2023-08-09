b2853f88230c961bf7277c942b1b4c9f30bd4688
narojisivamurthi <61727026+narojisivamurthi@users.noreply.github.com>
2023-08-09 15:07:04 +0530

    Add files via upload

---
+++ theme.ts
@@ -0,0 +1,48 @@
+export const theme = {
+    primary: {
+        THEME: '#231717',
+        SECONDARY_THEME: '#263757'
+    },
+    standard: {
+        WHITE: '#ffffff',
+        DARK_BLACK: '#000000',
+        LIGHT_BLACK: '#2C282814',
+        BRIGHT_BLACK: '#3F404F'
+    },
+    info: {
+        DARK: '#4C86ED',
+        DARK_INFO: '#5583D9',
+        LIGHT_INFO: '#4164A6'
+    },
+    danger: {
+        DARK_DANGER: '#dc3545'
+    },
+    warning: {
+        DARK_WARNING: '#F7B64A',
+        LIGHT_WARNING: '#F0F0F0'
+    },
+    success: {
+        DARK_SUCCESS: '#43B584',
+        LIGHT_SUCCESS: '#71a6a0'
+    },
+    light_primary: {
+        LIGHT_PRIMARY: '#AEB9C7',
+        LIGHT_SECONDARY: '#757788',
+        LIGHT_TERTIARY: '#57647E',
+        LIGHT_QUATERNARY: '#B2C3D9',
+        LIGHT_QUINARY: '#424D69',
+        LIGHT_SENARY: '#868686',
+        LIGHT_OCTONARY: '#90A0B7',
+        LIGHT_NONARY: '#65738E',
+        LIGHT_DENARY: '#7C7C7C'
+    },
+    light_secondary: {
+        LIGHT_PRIMARY: '#F7F7F7',
+        LIGHT_SECONDARY: '#4B74BE1A',
+        LIGHT_DARK: '#DDDDDD',
+        BRIGHT_DARK: '#F5F5F5'
+    },
+    tooltip: {
+        LIGHT_BORDER: 'rgb(178 195 217 / 34%)'
+    }
+};
